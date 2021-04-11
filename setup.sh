1ecb18c6080578dab970ac0d57de597828e0521e
rupeshyadav08 <47237661+rupeshyadav08@users.noreply.github.com>
2021-04-11 13:03:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p /app/.streamlit/echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > /app/.streamlit/config.toml
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
